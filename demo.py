ac66e6e436d592a837a8751c76553d65abfe44ca
Joshua Franklin <mail@joshfrankl.in>
2018-10-08 13:45:33 -0600

    Commented on demo.py.

--- demo.py
+++ demo.py
@@ -1,11 +1,13 @@
 from SpriteHelper import start, load, display
 from time import sleep
 
+# This assumes we have a sprite sheet called explosion.png that is a 9x9 animation with each frame being 100x100 pixels.
 image = load("explosion.png")
-size = 1000
+size = 100
 num_rows = 9
 num_cols = 9
 
+# This window is tiny, but big enough to hold the animation.
 start(size=[size, size])
 
 for row in range(0, size*num_rows, size):
