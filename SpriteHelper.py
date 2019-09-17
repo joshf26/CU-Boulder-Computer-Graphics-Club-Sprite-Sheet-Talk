337506aee09d264c70f57c56456495ac08aca675
Joshua Franklin <joshuafranklin26@gmail.com>
2019-09-16 21:50:34 -0600

    Update api and demo to make things simpler

--- SpriteHelper.py
+++ SpriteHelper.py
@@ -13,57 +13,50 @@ def _event_loop():
                 run = False
 
 
-def start(size=None, window_name='Sprite Helper'):
-    """ Opens the pygame window and keeps it open. """
+class Screen:
+    def __init__(self, size=None, window_name='Sprite Helper'):
+        if size is None:
+            size = [640, 480]
 
-    global screen
+        self.images = {}
 
-    if size is None:
-        size = [640, 480]
+        self.x = 0
+        self.y = 0
 
-    # Setup pygame window.
-    pygame.init()
-    pygame.display.set_caption(window_name)
-    screen = pygame.display.set_mode(size)
-    pygame.display.flip()
+        # Setup pygame window.
+        pygame.init()
+        pygame.display.set_caption(window_name)
+        self.screen = pygame.display.set_mode(size)
+        pygame.display.flip()
 
-    # Start event loop in another thread to keep the pygame window open.
-    threading.Thread(target=_event_loop).start()
+        # Start event loop in another thread to keep the pygame window open.
+        threading.Thread(target=_event_loop).start()
 
+    def __call__(self, path, crop=None, scale=1):
+        # Check if the image has already been loaded.
+        if path not in self.images:
+            self.images[path] = pygame.image.load(path)
 
-def load(path):
-    """ Wrapper around pygame's image load function. """
+        image = self.images[path]
 
-    image = pygame.image.load(path)
-    return image
+        # Clear the screen.
+        self.screen.fill([0, 0, 0])
 
+        # Apply scaling.
+        if not scale == 1:
+            image = pygame.transform.scale(image, (
+                int(image.get_width() * scale),
+                int(image.get_height() * scale)
+            ))
+            # x_pos = self.x * scale
+            # y_pos = self.y * scale
 
-def display(image, position=None, crop=None, scale=1):
-    """ Displays an image on the screen. """
+        # Apply cropping.
+        if crop:
+            crop = [int(i * scale) for i in crop]
+        else:
+            crop = [0, 0, image.get_width(), image.get_height()]
 
-    global screen
-
-    if position is None:
-        position = [0, 0]
-
-    # Clear the screen.
-    screen.fill([0, 0, 0])
-
-    # Apply scaling.
-    if not scale == 1:
-        image = pygame.transform.scale(image, (
-            int(image.get_width() * scale),
-            int(image.get_height() * scale)
-        ))
-        position[0] *= scale
-        position[1] *= scale
-
-    # Apply cropping.
-    if crop:
-        crop = [int(i * scale) for i in crop]
-    else:
-        crop = [0, 0, image.get_width(), image.get_height()]
-
-    # Display the image.
-    screen.blit(image, position, crop)
-    pygame.display.flip()
+        # Display the image.
+        self.screen.blit(image, [self.x, self.y], crop)
+        pygame.display.flip()
--- demo.py
+++ demo.py
@@ -1,17 +1,33 @@
-from SpriteHelper import start, load, display
+from SpriteHelper import Screen
 from time import sleep
 
-# This assumes we have a sprite sheet called explosion.png that is a 9x9 animation with each frame being 100x100 pixels.
-image = load("explosion.png")
-size = 100
-num_rows = 9
-num_cols = 9
+# Create the screen.
+screen = Screen(window_name='Graphics Club!')
 
-# This window is tiny, but big enough to hold the animation.
-start(size=[size, size])
+# Animate the skeleton walking down.
+# This assumes you have a sprite sheet titled 'skeleton.png' in your directory.
+skeleton_size = 64
+skeleton_cols = 8
+for i in range(5):
+    for col in range(skeleton_size, skeleton_size*skeleton_cols, skeleton_size):
+        screen(
+            'skeleton.png',
+            crop=[col, skeleton_size * 2, skeleton_size, skeleton_size],
+            scale=4
+        )
+        screen.y += 5
+        sleep(0.1)
 
-for row in range(0, size*num_rows, size):
-    for col in range(0, size*num_cols, size):
-        display(image, crop=[col, row, size, size])
-        sleep(0.01)
-
+# Animate the explosion.
+# This assumes you have a sprite sheet titled 'explosion.png' in your directory.
+explosion_size = 100
+explosion_rows = 9
+explosion_cols = 9
+for row in range(0, explosion_size*explosion_rows, explosion_size):
+    for col in range(0, explosion_size*explosion_cols, explosion_size):
+        screen(
+            'explosion.png',
+            crop=[col, row, explosion_size, explosion_size],
+            scale=(skeleton_size/explosion_size)*4
+        )
+        sleep(0.001)
